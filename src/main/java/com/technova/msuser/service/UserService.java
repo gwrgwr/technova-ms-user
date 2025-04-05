6feaba54772648df7ee0c43cee2e8e1bd2562d29
gwrgwr <murilomassa72@gmail.com>
2025-04-04 21:18:52 -0300

    add: new queue finding user by email

--- src/main/java/com/technova/msuser/service/UserService.java
+++ src/main/java/com/technova/msuser/service/UserService.java
@@ -1,11 +1,15 @@
 package com.technova.msuser.service;
 
-import com.technova.dto.UserResponseDTO;
+import com.technova.dto.user.UserLoginResponse;
+import com.technova.dto.user.UserResponseDTO;
 import com.technova.msuser.domain.UserEntity;
 import com.technova.msuser.repository.UserRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserService {
@@ -22,7 +26,15 @@ public class UserService {
         return new UserResponseDTO(true, "User saved successfully");
     }
 
-    public UserEntity getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    @RabbitListener(queues = "user-login-request")
+    public UserLoginResponse getUserByEmail(String email) {
+        UserEntity user = userRepository.findByEmail(email);
+        if (user != null) {
+            Map<String, String> map = new HashMap<>();
+            map.put("email", user.getEmail());
+            map.put("password", user.getPassword());
+            return new UserLoginResponse(true, map);
+        }
+        return new UserLoginResponse(false, null);
     }
 }
